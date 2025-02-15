2b5d8ab4d6ab04eb72afa4a506d5c0522992feed
Anjali Yadav <anjali7788y@gmail.com>
2025-02-15 19:08:37 +0530

    Added project files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Joke from './Joke'
+
+function App() {
+  return (
+    <>
+    <Joke/>
+    </>
+  )
+}
+
+export default App
